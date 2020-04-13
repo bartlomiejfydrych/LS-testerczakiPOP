e6777a80c16d1763ef721658d261b6afa0852d48
bartlomiejfydrych <fydbar@gmail.com>
2020-04-13 11:11:56 +0200

    Add methods to AsercjaPage

--- src/test/java/pages/AsercjaPage.java
+++ src/test/java/pages/AsercjaPage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -18,5 +19,28 @@ public class AsercjaPage extends BasePage {
     @FindBy(id = "city") WebElement miastoInput;
     @FindBy(id = "rulesLabel") WebElement regulaminChceckbox;
     @FindBy(id = "submit") WebElement wyslijButton;
-    
+
+    public void wpiszImie(String imie){
+        imieInput.sendKeys(imie);
+        Assert.assertEquals(imie, imieInput.getAttribute("value"));
+    }
+    public void wpiszNazwisko(String nazwisko){
+        nazwiskoInput.sendKeys(nazwisko);
+        Assert.assertEquals(nazwisko, nazwiskoInput.getAttribute("value"));
+    }
+    public void wpiszEmail(String email){
+        emailInput.sendKeys(email);
+        Assert.assertEquals(email, emailInput.getAttribute("value"));
+    }
+    public void wpiszMiasto(String miasto){
+        miastoInput.sendKeys(miasto);
+        Assert.assertEquals(miasto, miastoInput.getAttribute("value"));
+    }
+    public void akceptujRegulamin(){
+        regulaminChceckbox.click();
+        Assert.assertTrue(regulaminChceckbox.isSelected());
+    }
+    public void wyslijDane(){
+        wyslijButton.click();
+    }
 }
