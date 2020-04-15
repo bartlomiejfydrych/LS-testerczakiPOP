4470fc4a691e3461c5a02aa0f3d7f4238245a649
bartlomiejfydrych <fydbar@gmail.com>
2020-04-15 15:32:44 +0200

    Napisano OpcjonalnyFormularzTest

--- src/test/java/tests/OpcjonalnyFormularzTest.java
+++ src/test/java/tests/OpcjonalnyFormularzTest.java
@@ -1,4 +1,23 @@
 package tests;
 
-public class OpcjonalnyFormularzTest {
+import org.junit.Test;
+import pages.GlownaPage;
+import pages.OpcjonalnyFormularzPage;
+
+public class OpcjonalnyFormularzTest extends BaseTest {
+
+    @Test
+    public void poprawneLogowanieTest(){
+        GlownaPage glownaPage = new GlownaPage(driver);
+        glownaPage.goToOpcjonalnyFormularzPage();
+        OpcjonalnyFormularzPage opcjonalnyFormularzPage = new OpcjonalnyFormularzPage(driver);
+        opcjonalnyFormularzPage.wpiszImie("Krzysiek");
+        opcjonalnyFormularzPage.wpiszNazwisko("Frank");
+        opcjonalnyFormularzPage.wpiszEmail("henryk23@wp.pl");
+        opcjonalnyFormularzPage.zaznaczDodatkowePola();
+        opcjonalnyFormularzPage.wpiszMiasto("Katowice");
+        opcjonalnyFormularzPage.wpiszUlica("Jana Pawła 2b/13");
+        opcjonalnyFormularzPage.wyslijDane();
+        opcjonalnyFormularzPage.sprawdzAlert();
+    }
 }
