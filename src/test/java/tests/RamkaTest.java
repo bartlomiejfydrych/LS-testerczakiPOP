3c55fc7ff05858e5130abb8a6377e3310b600e12
bartlomiejfydrych <fydbar@gmail.com>
2020-04-17 13:33:57 +0200

    Napisano RamkaTest

--- src/test/java/tests/RamkaTest.java
+++ src/test/java/tests/RamkaTest.java
@@ -1,4 +1,26 @@
 package tests;
 
-public class RamkaTest {
+import org.junit.Test;
+import pages.GlownaPage;
+import pages.PodstawyPage;
+import pages.RamkaPage;
+
+public class RamkaTest extends BaseTest {
+
+    @Test
+    public void poprawneLogowanieTest(){
+        GlownaPage glownaPage = new GlownaPage(driver);
+        glownaPage.goToRamkaPage();
+        RamkaPage ramkaPage = new RamkaPage(driver);
+        ramkaPage.przejdzDoIframeOne();
+        ramkaPage.wpiszImie("Bartek");
+        ramkaPage.wpiszNazwisko("Bogucki");
+        ramkaPage.wpiszEmail("saturn993@amorki.pl");
+        ramkaPage.wpiszMiasto("Radom");
+        ramkaPage.wpiszUlica("Kochanowskiego 4a/5");
+        ramkaPage.akceptujRegulamin();
+        ramkaPage.wyslijDane();
+        ramkaPage.sprawdzAlert();
+        ramkaPage.wrocDoDomyslnegoOkna();
+    }
 }
