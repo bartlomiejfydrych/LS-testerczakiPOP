be8eb296bd533eefc020ea0ae11c83e5045eeacc
bartlomiejfydrych <fydbar@gmail.com>
2020-04-18 12:39:57 +0200

    Utworzono puste Menu Page i Test

---
+++ src/test/java/pages/MenuPage.java
@@ -0,0 +1,11 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class MenuPage extends BasePage {
+
+    public MenuPage(WebDriver driver) {
+        super(driver);
+    }
+    
+}
