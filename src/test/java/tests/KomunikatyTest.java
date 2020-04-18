963db8871d6b9ef43051c0eedca1ccf3ba14199f
bartlomiejfydrych <fydbar@gmail.com>
2020-04-18 11:11:37 +0200

    Napisano KomunikatyTest

--- src/test/java/tests/KomunikatyTest.java
+++ src/test/java/tests/KomunikatyTest.java
@@ -1,4 +1,26 @@
 package tests;
 
-public class KomunikatyTest {
+import org.junit.Test;
+import pages.GlownaPage;
+import pages.KomunikatyPage;
+
+public class KomunikatyTest extends BaseTest {
+
+    @Test
+    public void poprawnyKomunikat(){
+        GlownaPage glownaPage = new GlownaPage(driver);
+        glownaPage.goToKomunikatyPage();
+        KomunikatyPage komunikatyPage = new KomunikatyPage(driver);
+        komunikatyPage.kliknijWyswietlKomunikat();
+        komunikatyPage.przejdzNaAlert();
+        komunikatyPage.akceptujAlert();
+        komunikatyPage.pobierzTekstAlert();
+        komunikatyPage.wklejKomunikat();
+        komunikatyPage.kliknijSprawdzKomunikat();
+        komunikatyPage.akceptujAlert();
+        komunikatyPage.odrzucAlert();
+        komunikatyPage.wyslijTekstDoAlert("tak");
+        komunikatyPage.akceptujAlert();
+        komunikatyPage.sprawdzPoprawnosc();
+    }
 }
