d4f944eaf7daee92b10c842a9684f232e4cc1e2e
bartlomiejfydrych <fydbar@gmail.com>
2020-04-19 16:20:25 +0200

    Napisano KliknijPrzytrzymajTest

--- src/test/java/tests/KliknijPrzytrzymajTest.java
+++ src/test/java/tests/KliknijPrzytrzymajTest.java
@@ -1,4 +1,37 @@
 package tests;
 
-public class KliknijPrzytrzymajTest {
+import org.junit.Test;
+import pages.GlownaPage;
+import pages.KliknijPrzytrzymajPage;
+
+public class KliknijPrzytrzymajTest extends BaseTest {
+
+    @Test
+    public void zadanie1Test(){
+        GlownaPage glownaPage = new GlownaPage(driver);
+        glownaPage.goToKliknijPrzytrzymajPage();
+        KliknijPrzytrzymajPage kliknijPrzytrzymajPage = new KliknijPrzytrzymajPage(driver);
+        kliknijPrzytrzymajPage.kliknijPrawym();
+        kliknijPrzytrzymajPage.kliknijOpcje2();
+        kliknijPrzytrzymajPage.sprawdzAlert1();
+    }
+    @Test
+    public void zadanie2Test(){
+        GlownaPage glownaPage = new GlownaPage(driver);
+        glownaPage.goToKliknijPrzytrzymajPage();
+        KliknijPrzytrzymajPage kliknijPrzytrzymajPage = new KliknijPrzytrzymajPage(driver);
+        kliknijPrzytrzymajPage.przytrzymajPrzycisk();
+        kliknijPrzytrzymajPage.sprawdzAlert2();
+    }
+    @Test
+    public void wszystkieZadaniaTest(){
+        GlownaPage glownaPage = new GlownaPage(driver);
+        glownaPage.goToKliknijPrzytrzymajPage();
+        KliknijPrzytrzymajPage kliknijPrzytrzymajPage = new KliknijPrzytrzymajPage(driver);
+        kliknijPrzytrzymajPage.kliknijPrawym();
+        kliknijPrzytrzymajPage.kliknijOpcje2();
+        kliknijPrzytrzymajPage.sprawdzAlert1();
+        kliknijPrzytrzymajPage.przytrzymajPrzycisk();
+        kliknijPrzytrzymajPage.sprawdzAlert2();
+    }
 }
